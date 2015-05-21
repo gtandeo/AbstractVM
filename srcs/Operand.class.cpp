963a798e86f0c32ffde3a21087cccf19bd90ec9b
gtandeo <gtandeo@student.42.fr>
2015-05-21 20:36:48 +0200

    rendu

--- srcs/Operand.class.cpp
+++ srcs/Operand.class.cpp
@@ -1,26 +1,5 @@
 #include "Operand.class.hpp"
 
-Operand::Operand(void)
-{
-	return ;
-}
-
-Operand::Operand(Operand const &src)
-{
-	*this = src;
-	return ;
-}
-
-Operand::Operand(std::string const &value) : _value(value)
-{
-	return ;
-}
-
-Operand::~Operand(void)
-{
-	return ;
-}
-
 IOperand const * Operand::createOperand(eOperandType type, std::string const &value) const
 {
 	return tab[type](value);
@@ -28,25 +7,47 @@ IOperand const * Operand::createOperand(eOperandType type, std::string const &va
 
 IOperand const * Operand::createInt8(std::string const &value) const
 {
-	return new Operand(value);
+	Operand		*ret;
+
+	ret = new Operand(value);
+	_type = INT8;
+	return ret;
 }
 
 IOperand const * Operand::createInt16(std::string const &value) const
 {
-	;
+	Operand		*ret;
+
+	ret = new Operand(value);
+	_type = INT16;
+	return ret;
 }
 
 IOperand const * Operand::createInt32(std::string const &value) const
 {
-	;
+	Operand		*ret;
+
+	ret = new Operand(value);
+	_type = INT32;
+	return ret;
 }
 
 IOperand const * Operand::createFloat(std::string const &value) const
 {
-	;
+	Operand		*ret;
+
+	ret = new Operand(value);
+	_type = FLOAT;
+	return ret;
 }
 
 IOperand const * Operand::createDouble(std::string const &value) const
 {
-	;
+	Operand		*ret;
+
+	//if (value > sizeof(double))
+	//	throw std::exception();
+	ret = new Operand(value);
+	_type = DOUBLE;
+	return ret;
 }

de314d8429c4e6876f2e57286d1c6ef715557f92
Gurvan TANDEO <gtandeo@e3r3p21.42.fr>
2015-05-24 04:54:17 +0200

    modulo OK + gestion div/mod error

--- srcs/parsing.cpp
+++ srcs/parsing.cpp
@@ -131,7 +131,10 @@ void	Parsing::div(void)
 	tmp = _container.top();
 	_container.pop();
 	if (_container.top()->toString() == "0")
+	{
+		_container.push(tmp);
 		throw Parsing::DivModException();
+	}
 	result = *tmp / *_container.top();
 	_container.pop();
 	_container.push(f.createOperand(result->getType(), result->toString()));
@@ -142,19 +145,20 @@ void	Parsing::mod(void)
 {
 	Factory				f;
 	IOperand const		*tmp;
-	eOperandType		e;
-	std::stringstream	ss;
+	IOperand const		*result;
 
 	if (_container.size() < 2)
 		throw Parsing::LessThatTwoValuesException();
 	tmp = _container.top();
 	_container.pop();
 	if (_container.top()->toString() == "0")
+	{
+		_container.push(tmp);
 		throw Parsing::DivModException();
-	ss << std::fmod(std::stod(tmp->toString().c_str()), std::stod(_container.top()->toString().c_str()));
-	e = tmp->getType() >= _container.top()->getType() ? tmp->getType() : _container.top()->getType();
+	}
+	result = *tmp % *_container.top();
 	_container.pop();
-	_container.push(f.createOperand(e, ss.str()));
+	_container.push(f.createOperand(result->getType(), result->toString()));
 }
 
 void	Parsing::print(void)
